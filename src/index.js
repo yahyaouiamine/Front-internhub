675157df74cef76211e888e78f0e13cb4b956ff9
yahyaouiamine <mohamedamineyahyaoui17@gmail.com>
2024-12-10 10:25:03 +0100

    Movies app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom' ;
+import App from './App';
+
+createRoot(document.getElementById('root')).render(<App />);
